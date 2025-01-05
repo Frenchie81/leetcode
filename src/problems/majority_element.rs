a4250e913f706e2cf88fa79c11ab78a39de01e5b
Brian French <bfrench9481@gmail.com>
2025-01-04 23:02:26 -0500

    try the boyer moore algo after finding it

--- src/problems/majority_element.rs
+++ src/problems/majority_element.rs
@@ -12,6 +12,25 @@ impl Solution {
         let majority = counts.iter().max_by(|a, b| a.1.cmp(b.1));
         *majority.unwrap().0
     }
+
+    // not my solution but was curious about a solution without a HashMap
+    pub fn majority_element_boyer_moore(nums: Vec<i32>) -> i32 {
+        let mut current = nums[0];
+        let mut votes = 1;
+
+        (1..nums.len()).for_each(|i| {
+            let num = nums[i];
+            if num == current {
+                votes += 1;
+            } else if votes == 0 {
+                current = num;
+            } else {
+                votes -= 1;
+            }
+        });
+
+        current
+    }
 }
 
 #[cfg(test)]
@@ -32,6 +51,24 @@ mod tests {
         let nums = vec![2, 2, 1, 1, 1, 2, 2];
 
         let output = Solution::majority_element(nums);
+
+        assert_eq!(2, output);
+    }
+
+    #[test]
+    fn example1_boyer_moore() {
+        let nums = vec![3, 2, 3];
+
+        let output = Solution::majority_element_boyer_moore(nums);
+
+        assert_eq!(3, output);
+    }
+
+    #[test]
+    fn example_boyer_moore() {
+        let nums = vec![2, 2, 1, 1, 1, 2, 2];
+
+        let output = Solution::majority_element_boyer_moore(nums);
 
         assert_eq!(2, output);
     }

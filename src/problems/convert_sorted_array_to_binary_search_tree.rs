33975cf214c99f7d706a9eb56c343c469c48e394
Brian French <bfrench9481@gmail.com>
2025-01-17 13:38:36 -0500

    solve convert sorted array to binary search tree

--- src/problems/convert_sorted_array_to_binary_search_tree.rs
+++ src/problems/convert_sorted_array_to_binary_search_tree.rs
@@ -24,7 +24,22 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        None
+        Solution::recurse(&nums)
+    }
+
+    fn recurse(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if nums.is_empty() {
+            return None;
+        }
+
+        let middle = nums.len() / 2;
+        let node = TreeNode {
+            val: nums[middle],
+            left: Solution::recurse(&nums[0..middle]),
+            right: Solution::recurse(&nums[middle + 1..nums.len()]),
+        };
+
+        Some(Rc::new(RefCell::new(node)))
     }
 }
 
@@ -32,10 +47,57 @@ impl Solution {
 mod tests {
     use super::*;
 
+    fn wrap_node(n: TreeNode) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Rc::new(RefCell::new(n)))
+    }
+
     #[test]
     fn example1() {
         let input = vec![-10, -3, 0, 5, 9];
 
         let output = Solution::sorted_array_to_bst(input);
+
+        let expected = wrap_node(TreeNode {
+            val: 0,
+            left: wrap_node(TreeNode {
+                val: -3,
+                left: wrap_node(TreeNode {
+                    val: -10,
+                    left: None,
+                    right: None,
+                }),
+                right: None,
+            }),
+            right: wrap_node(TreeNode {
+                val: 9,
+                left: wrap_node(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }),
+                right: None,
+            }),
+        });
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn example2() {
+        let input = vec![1, 3];
+
+        let output = Solution::sorted_array_to_bst(input);
+
+        let expected = wrap_node(TreeNode {
+            val: 3,
+            left: wrap_node(TreeNode {
+                val: 1,
+                left: None,
+                right: None,
+            }),
+            right: None,
+        });
+
+        assert_eq!(expected, output);
     }
 }
